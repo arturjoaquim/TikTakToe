571535959744e9ead932bcd39c5f8cd0813698ec
Artur Joaquim <artur.joaquimbr@gmail.com>
2023-12-09 09:35:52 -0300

    Add getters in Player class and method playRound

--- tiktaktoe/Player.java
+++ tiktaktoe/Player.java
@@ -1,27 +1,54 @@
 package tiktaktoe;
+import java.util.Scanner;
 
 public class Player {
     
     private static int playersAmount = 0; 
 
-    private int wins;
-    private int loses;
-    private int id;
+    private byte wins;
+    private byte loses;
+    private byte id;
     private String name;
 
     public Player(String name) {
+        playersAmount++;
+        this.name = name;
         this.wins = 0;
         this.loses = 0;
-        this.id = playersAmount++;
+        this.id = (byte) (playersAmount);
+    }
+
+    public int playRound() {
+        Scanner scan = new Scanner(System.in);
+        return scan.nextInt();
     }
 
     public int getId() {
         return id;
     }
 
+    public byte getWins() {
+        return wins;
+    }
+
+    public byte getLoses() {
+        return loses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static int getPlayersAmount() {
+        return playersAmount;
+    }
+
     public static void main (String[] args) {
         Player firstPlayer = new Player("Artur");
-        var playerID = get   
-        
+        var playerID = firstPlayer.getId();
+        System.out.println("O ID do player 1 é " + playerID);
+        System.out.println("Quantia de players " + Player.getPlayersAmount());
+        Player secondPlayer = new Player("João");
+        System.out.println(getPlayersAmount());
     }
 }
