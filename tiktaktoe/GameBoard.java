cd6a1882e42d5a8d4664bb3cd1b67c0bbbaf7b4c
Artur Joaquim <artur.joaquimbr@gmail.com>
2023-12-09 09:40:21 -0300

    Add Dependece Injection in TikTakToe class

--- tiktaktoe/GameBoard.java
+++ tiktaktoe/GameBoard.java
@@ -4,10 +4,15 @@ public class GameBoard {
 
     private int[][] gameTable = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
+    public int[][] getGameTable() {
+        return gameTable;
+    }
+
     public static void main (String[] args) {
         GameBoard tabuleiro = new GameBoard();
-        System.out.println(getTableSize());
-
+        int[][] table = tabuleiro.getGameTable();
+        System.out.println(table.length);
+        System.out.println(table[0][1] == 1);
     }
 
     
--- tiktaktoe/TikTakToe.java
+++ tiktaktoe/TikTakToe.java
@@ -2,5 +2,11 @@ package tiktaktoe;
 
 public class TikTakToe {
     
-    private Player[]
+    private Player[] players;
+    private GameBoard board;
+
+    public TikTakToe(Player[] players, GameBoard board) {
+        this.players = players;
+        this.board = board;
+    }
 }
